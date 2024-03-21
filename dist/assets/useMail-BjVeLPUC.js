708a2f7fc38bf881a4ca9580304372105d496690
Aradhya Shaswat <reddye1120@gmail.com>
2024-03-22 01:39:48 +0530

    asd

---
+++ dist/assets/useMail-BjVeLPUC.js
@@ -0,0 +1,1 @@
+import{e as i}from"./actions-Bpk_XOR4.js";import{aV as n,b8 as r}from"./index-BpW8e4D6.js";import{h as c}from"./selectors-C4bJO8cF.js";function d({entity:s}){const{isLoading:t}=n(c),o=r();return{send:a=>{const e={id:a};o(i.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
--- dist/assets/useMail-HqlA0XX9.js
+++
@@ -1,1 +0,0 @@
-import{e as i}from"./actions-Bd3ByTEX.js";import{aV as n,b8 as r}from"./index-Ct6RrEec.js";import{h as c}from"./selectors-4q4i6c4u.js";function d({entity:s}){const{isLoading:t}=n(c),o=r();return{send:a=>{const e={id:a};o(i.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
